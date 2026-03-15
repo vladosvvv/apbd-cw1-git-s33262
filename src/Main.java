46fe105ebf006498921e731f0122ba8003b5c501
Vladyslav Datsiuk <s33262@pjwstk.edu.pl>
2026-03-15 12:52:39 +0100

    Add personalized greeting

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,17 @@
 import java.util.Scanner;
 
-public class Main{
-    public static void main(String[] args){
-        System.out.println("Hello world!");
+public class Main {
+    public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+
         System.out.print("Enter your name: ");
         String name = scanner.nextLine();
+
         if (name.isBlank()) {
             System.out.println("Name cannot be empty.");
             return;
         }
+
+        System.out.println("Hello, " + name + "!");
     }
 }
