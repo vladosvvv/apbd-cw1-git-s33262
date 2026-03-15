79680d90e6120b1e76f19027fd12591e933c4646
Vladyslav Datsiuk <s33262@pjwstk.edu.pl>
2026-03-15 12:39:20 +0100

    Add empty input validation

--- src/Main.java
+++ src/Main.java
@@ -3,8 +3,12 @@ import java.util.Scanner;
 public class Main{
     public static void main(String[] args){
         System.out.println("Hello world!");
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter your name: ");
+        String name = scanner.nextLine();
+        if (name.isBlank()) {
+            System.out.println("Name cannot be empty.");
+            return;
+        }
     }
-    Scanner scanner = new Scanner(System.in);
-    System.out.print("Enter your name: ");
-    String name = scanner.nextLine();
 }
