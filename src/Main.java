0c90e9ec770125c2609cbd3e3b60c1c32ed42b4d
Vladyslav Datsiuk <s33262@pjwstk.edu.pl>
2026-03-15 13:32:12 +0100

    Add validation to calculateMin

--- src/Main.java
+++ src/Main.java
@@ -39,6 +39,10 @@ public class Main {
     }
 
     public static int calculateMin(int[] values) {
+        if (values == null || values.length == 0) {
+            throw new IllegalArgumentException("Array cannot be empty.");
+        }
+
         int min = values[0];
 
         for (int value : values) {
